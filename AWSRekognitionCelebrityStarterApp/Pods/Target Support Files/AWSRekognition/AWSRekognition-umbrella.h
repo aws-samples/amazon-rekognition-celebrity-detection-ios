0d110288a1b131461594e8421e3b6229e795c3b8
pawanp3 <p.pawan@gmail.com>
2018-10-04 15:41:26 -0700

    Modified original files

---
+++ AWSRekognitionCelebrityStarterApp/Pods/Target Support Files/AWSRekognition/AWSRekognition-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AWSRekognition.h"
+#import "AWSRekognitionModel.h"
+#import "AWSRekognitionResources.h"
+#import "AWSRekognitionService.h"
+
+FOUNDATION_EXPORT double AWSRekognitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char AWSRekognitionVersionString[];
+
---
+++ AWSRekognitionCelebrityStarterApp/Pods/Target Support Files/Pods-AWSRekognitionStarterAppUITests/Pods-AWSRekognitionStarterAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWSRekognitionStarterAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AWSRekognitionStarterAppUITests
+@end
